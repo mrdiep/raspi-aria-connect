bf10bcb275e667228c8e9e20ee3b0427e7c8ea5a
DiepNV4 <DiepNV4@fsoft.com.vn>
2019-06-27 16:29:09 +0700

    Code

--- aria2-connect-host/src/app.js
+++ aria2-connect-host/src/app.js
@@ -8,6 +8,12 @@ console.log('start server');
 socket.on('connect', async () => {
   console.log('connected');
 
+  const methods = await aria2.listMethods();
+  var files = await aria2.call("getFiles", 'ab4eb47b5f3d7369');
+  console.log(JSON.stringify(files, null, 4));
+
+  var status = await aria2.call('tellStatus', 'ab4eb47b5f3d7369');
+  console.log(status ,methods)
   var e = await aria2.open();
   const notifications = await aria2.listNotifications();
   notifications.forEach((notification) => {
