191b0b5155a6ceb6d03b8b0a12ba6bdc78eaf6ca
DiepNV4 <DiepNV4@fsoft.com.vn>
2019-06-27 13:27:46 +0700

    Init all

---
+++ aria2-connect-dispatcher/src/app.js
@@ -0,0 +1,20 @@
+import express from  'express';
+import { Server } from 'http';
+import socket from 'socket.io';
+
+var app = express();
+var server = Server(app);
+var io = socket(server);
+
+server.listen(8081);
+console.log('listen on localhost:8081')
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
